97765cbce59c8bf4ac4cc9f043e03e9564abd47a
Uduthamahesh77 <208r1a66b4@gmail.com>
2024-01-08 14:09:16 +0530

    found a max of element in a array

--- Array.java
+++ Array.java
@@ -3,9 +3,13 @@ import java.lang.*;
 public class Array {
     public static void main(String[] args) {
         int array[]={1,2,3,4,5,6};
+        int x=0;
         for(int i=0;i<array.length;i++){
             System.out.print(array[i]+" ");
+            x=Math.max(x,array[i]);
         }
+        System.out.println();
+        System.out.println(x);
         
     }
 }
