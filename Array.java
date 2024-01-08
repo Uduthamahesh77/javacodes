e65f0e1a4c1bd35e2ab6a88d93bf270ff0c3d98e
Uduthamahesh77 <208r1a66b4@gmail.com>
2024-01-08 13:49:11 +0530

    created a array file for array programs

---
+++ Array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class Array {
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5,6};
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        
+    }
+}
