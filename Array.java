19f7d6e6e4fe1b9965db0d33f53608568f28c777
Uduthamahesh77 <208r1a66b4@gmail.com>
2024-01-09 23:32:37 +0530

    added a program of trapping rain water

--- Array.java
+++ Array.java
@@ -64,10 +64,30 @@ public class Array {
             System.out.println();
         }
     }
+// a program calculating trappimg rain water..............................................................................................
+    public static int Trappingwater(int height[]){
+        int n=height.length;
+        int leftmax[]=new int[n];
+        leftmax[0]=height[0];
+        for(int i=1;i<n;i++){
+            leftmax[i]=Math.max(leftmax[i-1],height[i]);
+        }
+        int rightmax[]=new int[n];
+        rightmax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            rightmax[i]=Math.max(rightmax[i+1],height[i]);
+        }
+        int trappingwater=0;
+        for(int i=0;i<n;i++){
+            int waterlevel=Math.min(leftmax[i],rightmax[i]);
+            trappingwater+=waterlevel-height[i];
+        }
+        return trappingwater;
+    }
 
 
     public static void main(String[] args) {
-        int array[]={1,-2,6,-1,3};
-        System.out.println(kadanssum(array));
+        int array[]={4,2,0,6,3,2,5};
+        System.out.println(Trappingwater(array));
     }
 }
